0d22781f747d22877daca8d56d11399b3f916909
BaekYeonSun <55418359+BaekYeonSun@users.noreply.github.com>
2021-06-13 01:13:16 +0900

    Add files via upload

---
+++ HW04/Camera.cpp
@@ -0,0 +1,55 @@
+#include "Camera.h"
+
+const glm::vec3 Camera::center_position() const
+{
+  return  glm::vec3(position_[0] + front_dir_[0],
+               position_[1] + front_dir_[1],
+               position_[2] + front_dir_[2]);
+}
+
+// TODO: fill up the following functions properly 
+const glm::mat4  Camera::get_view_matrix()
+{
+  // return glm::lookAt(position_, position_ + front_dir_, up_dir_);
+  return glm::lookAt(position(), center_position(), up_direction());
+}
+
+void Camera::move_forward(float delta)
+{
+  // position_ += delta * front_dir_;
+  position_ += glm::normalize(front_dir_) * delta;
+}
+
+void Camera::move_backward(float delta)
+{
+  // position_ -= delta * front_dir_;
+  position_ -= glm::normalize(front_dir_) * delta;
+}
+
+void Camera::move_left(float delta)
+{
+  position_ -= glm::normalize(glm::cross(front_dir_, up_dir_)) * delta;
+}
+
+void Camera::move_right(float delta)
+{
+  position_ += glm::normalize(glm::cross(front_dir_, up_dir_)) * delta;
+}
+
+void Camera::move_up(float delta)
+{
+  // position_ += up_dir_ * delta;
+  position_ += glm::normalize(up_dir_) * delta;
+}
+
+void Camera::move_down(float delta)
+{
+  // position_ -= up_dir_ * delta;
+  position_ -= glm::normalize(up_dir_) * delta;
+}
+
+
+void Camera::update_front_direction(glm::vec3 dir)
+{
+  front_dir_ = dir - position_;
+}
